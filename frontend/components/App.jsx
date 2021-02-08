8152cdd2e66aadb4ec3e8e3f73465569b4f1fd5a
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-02-08 15:32:57 -0800

    working on frontend auth

---
+++ frontend/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import GreetingContainer from './greeting_container';
+import Modal from './modal/modal';
+import { Link, Route } from 'react-router-dom';
+const App = () => {
+    return (
+        <div>
+            <Modal />
+            <header>
+                <Link to='/'>
+                    <h1>Uniqsy</h1>
+                </Link>
+                <GreetingContainer />
+            </header>
+        </div>
+    );
+};
+
+export default App;
