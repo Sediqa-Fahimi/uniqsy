d75603ffaae95ee3aef3f2a20a369e047c4e782c
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-02-12 23:55:48 -0800

    Working on styling the home page

--- frontend/components/products/search.jsx
+++ frontend/components/products/search.jsx
@@ -4,10 +4,13 @@ const Search = () =>{
     return (
         <div className="search-container">
             <input type="text" className="search-input" placeholder="Search for anything"/>
-            <button className="search-btn" >O--</button>
+            <button className="search-btn" >
+                <center>
+                    <img src={window.searchURL} alt="" className="search-icon" />
+                </center>
+            </button>
         </div>
     )
 }
 export default Search;
 
-{/* <img src={window.searchURL} alt="" /> */}
