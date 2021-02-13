5d05504085ca04824dfebbfa7f11d48b07353d52
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-02-13 00:53:36 -0800

    Working on styling the home page

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -11,9 +11,14 @@ const App = () => {
             <div className="main-header">
                 <Modal />
                 <header className="inner-header">
-                    <Link to='/' className="icon">Uniqsy</Link>
+                    <Link to='/' className="home-icon">Uniqsy</Link>
                     <SearchContainer />
                     <HeaderContainer />
+                    <Link to='#' className="shopping-cart-link">
+                        <div className="shopping-cart-icon-container">
+                            <img src={window.cartURL} alt="" className="shopping-cart-icon" />
+                        </div>
+                    </Link>
                 </header>
                 
             </div>
