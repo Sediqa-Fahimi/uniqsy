f2fc3d6d3da2e8a20a94efdadbd65e4f9006bd9b
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-03-18 00:29:09 -0700

    style review form

--- frontend/util/link_util.jsx
+++ frontend/util/link_util.jsx
@@ -3,8 +3,8 @@ import { Route, Link } from 'react-router-dom';
 
 export const ReviewLink = ({ label, to }) => (
   <Route path={to} children={({ match }) => (
-    <div>
-      {match ? "" : <Link to={to}>{label}</Link>}
+    <div className="review-link">
+      {match ? "" : <Link to={to} className="review-L">{label}</Link>}
     </div>
   )} />
 );
