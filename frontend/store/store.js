669a9498a7e42b3476f792567ca12ecc919ccdf9
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-07-20 12:24:44 -0700

    add total reviews to product model

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -4,7 +4,7 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
 const configureStore = (preloadedState = {}) => {
-    return createStore(rootReducer, preloadedState, applyMiddleware(thunk));
+    return createStore(rootReducer, preloadedState, applyMiddleware(thunk,logger));
 }
 
 export default configureStore;
