b0a98736069f42c6d91a9c3286ec9e600451981e
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-07-23 15:57:33 -0700

    add reviews and styles to index listings

--- frontend/components/products/product_index_item.jsx
+++ frontend/components/products/product_index_item.jsx
@@ -1,36 +1,43 @@
 import React from 'react';
 import { withRouter, Link } from 'react-router-dom';
-import StarRatingComponent from 'react-star-rating-component';
+import {IoStarSharp} from 'react-icons/io5';
 
 
 class ProductIndexItem extends React.Component {
     constructor(props) {
         super(props);
-        this.handleClick = this.handleClick.bind(this);
+        
     }
-    handleClick(e) {
-      const {product, history} = this.props;
-      const productId = product.id;
-      history.push(`/products/${productId}`);
-  }
+    
     render() {
         const { product } = this.props;
+        const colors = {
+          black: '#444343',
+          grey: '#CFCFCF'
+        }
         return (
           <li key={product.id} >
-              <Link to={`/products/${product.id}`} onClick={()=> this.handleClick}>
+              <Link to={`/products/${product.id}`} >
                 <img src={product.photoUrl} alt=""/>
-                <span>
-                  <StarRatingComponent 
-                      name="average-rating"
-                      editing={false}
-                      starCount={5}
-                      value={parseFloat(product.averageRating)}
-                      starColor={"#222323"}
-                      emptyStarColor={"#DDDCDC"}
-                      starSpacing="15px"
-                  />
-                </span>
-                <span>({product.totalReviews})</span><br/>
+                <div className="listing-reviews">
+                  <span>
+                    {Array(5)
+                    .fill(0)
+                    .map((_,i)=>{
+                      return(
+                        <span>
+                          <IoStarSharp 
+                            key={i}
+                            size={16}
+                            color={product.averageRating >= i+1 ? colors.black : colors.grey}
+                          />
+                        </span>
+                      );
+                    })
+                    }
+                  </span>
+                  <span>({product.totalReviews})</span>
+                </div>
                 ${product.price}
               </Link>
           </li>
